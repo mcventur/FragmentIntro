a46af5df9ea7948d65091aea1a908bd45f6fb5f9
mcventur <marcelinopenide@gmail.com>
2023-10-30 13:31:31 +0100

    Comportamiento segundo fragment

--- app/src/main/java/com/mpd/pmdm/fragmentintro/ColorFragment.kt
+++ app/src/main/java/com/mpd/pmdm/fragmentintro/ColorFragment.kt
@@ -1,10 +1,13 @@
 package com.mpd.pmdm.fragmentintro
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +38,24 @@ class ColorFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_color, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val redButton = view.findViewById<Button>(R.id.red_button)
+        val greenButton = view.findViewById<Button>(R.id.green_button)
+        val blueButton = view.findViewById<Button>(R.id.blue_button)
+        val helloWorldTextView = view.findViewById<TextView>(R.id.hello_world)
+        redButton.setOnClickListener {
+            helloWorldTextView.setTextColor(Color.RED)
+        }
+        greenButton.setOnClickListener {
+            helloWorldTextView.setTextColor(Color.GREEN)
+        }
+        blueButton.setOnClickListener {
+            helloWorldTextView.setTextColor(Color.BLUE)
+        }
     }
 
     companion object {
